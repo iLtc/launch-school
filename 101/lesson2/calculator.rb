568b1b9fe9ab672314f0610a0144b02e99430a62
i <i@iltc.io>
2016-05-26 13:38:25 -0400

    Improve code with rebocop advice

--- 101/lesson2/calculator.rb
+++ 101/lesson2/calculator.rb
@@ -1,24 +1,24 @@
-puts "Welcome!"
+puts 'Welcome!'
 
-print "First Number: "
+print 'First Number: '
 num1 = gets.chomp
 
-print "Second Number: "
+print 'Second Number: '
 num2 = gets.chomp
 
-print "Operation (1-add 2-subtract 3-multiply 4-divide): "
+print 'Operation (1-add 2-subtract 3-multiply 4-divide): '
 operator = gets.chomp.to_i
 
-case operator
-  when 1
-    result = num1.to_i + num2.to_i
-  when 2
-    result = num1.to_i - num2.to_i
-  when 3
-    result = num1.to_i * num2.to_i
-  else
-    #num2 = 0?
-    result = num1.to_f / num2.to_f
-end
+result =  case operator
+          when 1
+            num1.to_i + num2.to_i
+          when 2
+            num1.to_i - num2.to_i
+          when 3
+            num1.to_i * num2.to_i
+          else
+            # num2 = 0?
+            num1.to_f / num2.to_f
+          end
 
 puts "The result is #{result}"
